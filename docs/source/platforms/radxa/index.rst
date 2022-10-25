0f6dcc91c9fa2edcc9bf4a7f5c5171936a0842b1
nascs <nascs@radxa.com>
2022-10-25 10:48:52 +0800

    Initial implementation of ROCK 3c platform and RK356X SOC

---
+++ docs/source/platforms/radxa/index.rst
@@ -0,0 +1,12 @@
+ROCK
+============
+
+.. toctree::
+   :maxdepth: 2
+
+   rock3a
+   rock3b
+   rock3c
+   radxa_cm3_io
+   radxa_e23
+   radxa_e25
--- python/setup.py
+++ python/setup.py
@@ -23,6 +23,7 @@ modules = [
 			'../src/soc/nxp/imx6sdlrm.c',
 			'../src/soc/nxp/imx6dqrm.c',
 			'../src/soc/samsung/exynos5422.c',
+			'../src/soc/rockchip/rk356x.c',
 			'../src/platform/platform.c',
 			'../src/platform/hardkernel/odroidc1.c',
 			'../src/platform/hardkernel/odroidc2.c',
@@ -42,7 +43,8 @@ modules = [
 			'../src/platform/solidrun/hummingboard_base_pro_dq.c',
 			'../src/platform/solidrun/hummingboard_base_pro_sdl.c',
 			'../src/platform/solidrun/hummingboard_gate_edge_dq.c',
-			'../src/platform/solidrun/hummingboard_gate_edge_sdl.c'
+			'../src/platform/solidrun/hummingboard_gate_edge_sdl.c',
+			'../src/platform/radxa/rock3c.c'
 		],
 		include_dirs=['../src/']
 	),
