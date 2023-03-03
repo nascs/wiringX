8128d14c50f63ffb26164c2929fbbd4394a39e4f
nascs <105425502+nascs@users.noreply.github.com>
2023-03-03 17:20:17 +0800

    update code style

--- src/platform/radxa/rock3c.h
+++ src/platform/radxa/rock3c.h
@@ -1,11 +1,12 @@
 /*
-	Copyright (c) 2022  Radxa Ltd.
-        Author: Nascs <nascs@radxa.com>
+	Copyright (c) 2023  Radxa Ltd.
+  Author: Nascs <nascs@radxa.com>
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+
 #ifndef __WIRINGX_ROCK3C_H_
 #define __WIRINGX_ROCK3C_H_
 
--- src/soc/rockchip/rk356x.h
+++ src/soc/rockchip/rk356x.h
@@ -1,6 +1,6 @@
 /*
-	Copyright (c) 2022 Radxa Ltd.
-        Author: Nascs <nascs@radxa.com>
+	Copyright (c) 2023 Radxa Ltd.
+  Author: Nascs <nascs@radxa.com>
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
